b5b63a49ab41e36fde849da2a7aaf546ce9738e0
Wikator <wiktor@szymulewicz.com>
2023-11-24 20:32:08 +0100

    Changed look of home page

--- RecipesApp/Helpers/PaginationParams.cs
+++ RecipesApp/Helpers/PaginationParams.cs
@@ -4,14 +4,24 @@
     {
         private const int _maxPageSize = 50;
 
-        public int PageNumber { get; set; }
+        private const int _minPageSize = 1;
+
+        private const int _minPageNumber = 1;
+
+        public int _pageNumber { get; set; }
+
+        public int PageNumber
+        {
+            get => _pageNumber;
+            set => _pageNumber = (value < _minPageNumber) ? _minPageNumber : value;
+        }
 
         private int _pageSize;
 
         public int PageSize
         {
             get => _pageSize;
-            set => _pageSize = (value > _maxPageSize) ? _maxPageSize : value;
+            set => _pageSize = (value > _maxPageSize) ? _maxPageSize : (value < _minPageSize) ? _minPageSize : value;
         }
 
         public string? OrderBy { get; set; }
